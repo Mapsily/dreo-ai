ef1dbe72278c48a0dc95d2d0c07240c44662db76
Saif Malik <saif.malik@mapsily.com>
2025-01-09 13:56:11 +0530

    fix

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,47 @@
+"use client";
+import useSideBar from "@/context/use-sidebar";
+import { cn } from "@/lib/utils";
+import React from "react";
+import MaxMenu from "./maximized-menu";
+import { MinMenu } from "./minimized-menu";
+
+type Props = {
+  domains:
+    | {
+        id: string;
+        name: string;
+        icon: string;
+      }[]
+    | null
+    | undefined;
+};
+
+const SideBar = ({ domains }: Props) => {
+  const { expand, onExpand, page, onSignOut } = useSideBar();
+
+  return (
+    <div
+      className={cn(
+        "bg-white dark:bg-muted h-full fill-mode-forwards fixed md:relative"
+      )}
+    >
+      {expand ? (
+        <MaxMenu
+          domains={domains}
+          current={page!}
+          onExpand={onExpand}
+          onSignOut={onSignOut}
+        />
+      ) : (
+        <MinMenu
+          domains={domains}
+          onShrink={onExpand}
+          current={page!}
+          onSignOut={onSignOut}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SideBar;
---
+++ src/components/sidebar/maximized-menu.tsx
@@ -0,0 +1,50 @@
+import { SIDE_BAR_MENU } from "@/constants/menu";
+import { LogOut, Menu } from "lucide-react";
+import React from "react";
+import DomainMenu from "./domain-menu";
+import MenuItem from "./menu-item";
+
+type Props = {
+  onExpand(): void;
+  current: string;
+  onSignOut(): void;
+  domains:
+    | {
+        id: string;
+        name: string;
+        icon: string | null;
+      }[]
+    | null
+    | undefined;
+};
+
+const MaxMenu = ({ current, domains, onExpand, onSignOut }: Props) => {
+  return (
+    <div className="py-3 px-4 flex flex-col h-full">
+      <div className="flex justify-between items-center">
+        Logo
+        <Menu onClick={onExpand} />
+      </div>
+      <div>
+        <div className="flex flex-col">
+          <p className="text-xs text-gray-500 mb-3">MENU</p>
+          {SIDE_BAR_MENU.map((menu, key) => (
+            <MenuItem size="max" {...menu} key={key} current={current} />
+          ))}
+          <DomainMenu domains={domains} />
+        </div>
+        <div className="flex flex-col">
+          <p className="text-xs text-gray-500 mb-3">OPTIONS</p>
+          <MenuItem
+            size="max"
+            label="Sign out"
+            icon={<LogOut />}
+            onSignOut={onSignOut}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MaxMenu;
